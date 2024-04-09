ba3945fd915b3afe99736a5fdfbe9b0009b57b69
Matias Espinoza <103543630+matiespi@users.noreply.github.com>
2024-04-09 00:02:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, world this is the project!' });
+    res.json({ message: 'Hello, world this is the projects!' });
 });
 
 const port = process.env.PORT || 3000;
